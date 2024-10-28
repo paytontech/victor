ce6c46a224d351d46c1f583d1bfc681855647365
Kerigan <kerigancreighton@gmail.com>
2024-10-28 17:41:09 -0500

    Set new server env, re-add snowglobe

--- animProcess/src/cozmoAnim/connectionFlow.cpp
+++ animProcess/src/cozmoAnim/connectionFlow.cpp
@@ -44,7 +44,7 @@ namespace {
 u32 _pin = 123456;
 
 const f32 kRobotNameScale = 0.6f;
-const std::string kURL = "keriganc.com/v";
+const std::string kURL = "v.pvic.xyz";
 const ColorRGBA   kColor(0.9f, 0.9f, 0.9f, 1.f);
 
 const char* kShowPinScreenSpriteName = "pairing_icon_key";
